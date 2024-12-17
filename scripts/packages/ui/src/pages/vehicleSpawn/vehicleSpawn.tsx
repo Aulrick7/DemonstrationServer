b34101a7a688e55247a3d7f0a8f043657576bc3b
selvakumar <selvakumarabhishek521@gmail.com>
2024-12-16 20:23:57 -0400

    yet

--- scripts/packages/ui/src/pages/vehicleSpawn/vehicleSpawn.tsx
+++ scripts/packages/ui/src/pages/vehicleSpawn/vehicleSpawn.tsx
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from 'react'
 import { usePageContext } from '../../App'
 import { fetchNui, useNuiQuery } from '../../utils/nui'
 import { useExitListener } from '../../utils/exitListener'
+import { ifError } from 'assert';
 
 const vehicleSpawn = () => {
     const { closePage } = usePageContext()
     const [vehicle, setVehicle] = React.useState('');
     const [refresh, setRefresh] = React.useState(false);
-
+    const [visible, setVisible] = React.useState(false);
+    // const app = express();
+    // app.use(cors());
+    
+    // const db = mysql.createConnection({
+    //     host: '127.0.0.1',
+    //     user: 'root',
+    //     password: '',
+    //     database: 'vehicles'
+    // })
+    // app.get('/vehicleSpawned', (req: any, res: { json: (arg0: any) => any; }) => {
+    //     const sql = "SELECT * FROM vehicleSpawned";
+    //     db.query(sql, (err: any, data: any) => {
+    //         if(err) return res.json(err)
+    //         return res.json(data); 
+    //     })
+    // })
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setVehicle(e.target.value);
     }
@@ -18,6 +35,8 @@ const vehicleSpawn = () => {
     const handleSubmit = () => {
         sendDataToFiveM(vehicle)
         setVehicle('');
+        addVehicle(vehicle)
+        close();
     }
     const sendDataToFiveM = (model: any) => {
         const data = {model}
@@ -27,12 +46,41 @@ const vehicleSpawn = () => {
     useExitListener(async () => {
       await close()
     })
-
+    
+    function addVehicle(model:String){
+      const ox = global.exports('oxmysql');
+      ox.execute('INSERT INTO vehicleSpawned (vehicleSpawned, dateSpawned) VALUES (?, CURRENT_TIMESTAMP)', model)
+    }
+  
     return (
       <div className='grid align-items h-screen place-content-center p-10 text-white'>
         <div className='bg-slate-700 p-12'>
-          <div><input type='text' placeholder='Enter a vehicle name' value={vehicle} onChange={handleInput}></input></div>
-          <button onClick={handleSubmit}>Submit</button>
+          <div>
+            <input type='text' placeholder='Enter a vehicle name' value={vehicle} onChange={handleInput} className='text-black'></input>
+            <button onClick={handleSubmit} className='p-3'>Submit</button>
+          </div>
+          {visible && (
+            <div>
+            History
+            <div className='vehicleSpawned'>
+                <table>
+                  <thead>
+                    <th>Vehicle Spawned</th>
+                    <th>Time Stamp</th>
+                  </thead>
+                  {/* <tbody>
+                    {data.map((d, i) => (
+                      <tr key={i}>
+                        <td>{d.vehicleSpawned}</td>
+                        <td>{d.timeStamp}</td>
+                      </tr>
+                    ))}
+                  </tbody> */}
+                </table>
+            </div>
+          </div>
+          )}
+          
         </div>
       </div>
     )
